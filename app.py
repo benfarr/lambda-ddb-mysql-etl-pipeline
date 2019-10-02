8ca37994cbe7ecf85bddda396b9ee8e0d790ecb1
Benjamin Farr <benfarr@bitbucket.org>
2019-10-01 18:29:16 -0600

    added dynamic env config, syntax cleanup and renamed stack and resources

---
+++ app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from aws_cdk import core
+from etl_pipeline_cdk.etl_pipeline_cdk_stack import EtlPipelineCdkStack
+import os, json
+
+PRE_PROD_ENV={
+	"region": "<region_id>",
+	"account": "<account_id>"
+}
+
+PROD_ENV={
+	"region": "<region_id>",
+	"account": "<account_id>"
+}
+
+STAGE=os.environ.get("stage")
+
+with open(f"params-{STAGE}.json") as file_obj:
+	stack_params=json.load(fil_obj)
+
+app = core.App()
+
+EtlPipelineCdkStack(app
+                     , stack_params['name'],
+                     , env=PRE_PROD_ENV if STAGE == "preprod" else PROD_ENV
+                     )
+app.synth()
